2310500edcb5b0313fef3b414508f6d1a6aaa021
Piero Angelo <piero.morales@utec.edu.pe>
2019-09-11 19:50:57 -0500

    ..

---
+++ cmake-build-debug/CMakeFiles/Traits.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Traits.dir/main.cpp.obj"
+  "Traits.pdb"
+  "Traits.exe"
+  "Traits.exe.manifest"
+  "libTraits.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Traits.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Traits.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Traits.
+# This may be replaced when dependencies are built.
